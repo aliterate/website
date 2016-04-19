49d94e88f214c6d6696c6ec664cc9b327b81a3dc
Brendan Hickey <brendan@bhickey.net>
2016-04-18 22:57:40 -0400

    Don't show the cookie notice if cookies are disabled.

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,8 +1,11 @@
-    window.cookieconsent_options = {"message":"This website, like many others, uses cookies.","dismiss":"OK","learnMore":"More info","link":"about/privacy","theme":"dark-bottom"};
+// Don't bother showing the cookie notice if the user is blocking cookies.
+if (navigator.cookieEnabled) {
+  window.cookieconsent_options = {"message":"This website, like many others, uses cookies.","dismiss":"OK","learnMore":"More info","link":"//www.aliterate.org/about/privacy/","theme":"dark-bottom"};
 
-var js = document.createElement("script");
+  var js = document.createElement("script");
 
-js.type = "text/javascript";
-js.src = "//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js";
+  js.type = "text/javascript";
+  js.src = "//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js";
 
-document.body.appendChild(js);
+  document.body.appendChild(js);
+}
